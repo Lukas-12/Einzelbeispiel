d9e0494b18a25d72a0d09535e0f06e889c080e20
lukas <lukasbu@edu.aau.at>
2021-03-06 12:52:17 +0100

    Design erstellt

---
+++ app/src/main/java/com/example/einzelbeispiel/TCPClient.java
@@ -0,0 +1,23 @@
+package com.example.einzelbeispiel;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPClient {
+    public static void main(String[] args)throws Exception {
+        int clientMatNr;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("se2-isys.aau.at",53212);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        clientMatNr = inFromServer.read();
+        outToServer.writeByte(clientMatNr);
+        System.out.println("FROM SERVER: " + clientMatNr);
+        clientSocket.close();
+
+    }
+}
--- app/src/main/java/com/example/einzelbeispiel/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.einzelbeispiel;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
-}
